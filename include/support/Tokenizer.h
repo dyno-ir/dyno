6e75a046a0909e4118d2be80417ba69af46629fc
Mathis <mathis.salmen@matsal.de>
2025-12-03 01:36:31 +0100

    missing file

---
+++ include/support/Tokenizer.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <cassert>
+#include <cstddef>
+#include <optional>
+#include <string>
+#include <string_view>
+
+class Tokenizer {
+  std::string_view s, delims;
+  size_t pos = 0;
+
+public:
+  Tokenizer(std::string_view str, std::string_view d = " ")
+      : s(str), delims(d) {}
+
+  class iterator {
+    Tokenizer *parent;
+    size_t pos;
+    size_t len;
+
+  public:
+    iterator &operator++() {
+      pos += len;
+      pos = parent->s.find_first_not_of(parent->delims, pos);
+      if (pos == std::string_view::npos) {
+        len = 0;
+        return *this;
+      }
+      size_t end = parent->s.find_first_of(parent->delims, pos);
+      if (end == std::string_view::npos)
+        end = parent->s.size();
+      len = end - pos;
+      return *this;
+    }
+
+    iterator operator++(int) {
+      auto copy{*this};
+      ++(*this);
+      return copy;
+    }
+
+    std::string_view operator*() { return parent->s.substr(pos, len); }
+
+    bool operator==(const iterator &other) const {
+      auto rv = other.parent == this->parent && other.pos == this->pos;
+      assert(!rv || this->len == other.len);
+      return rv;
+    }
+
+    iterator(Tokenizer *parent, size_t pos) : parent(parent), pos(pos), len(0) {
+      // first increment does not advance as len == 0, just primes.
+      ++(*this);
+    }
+  };
+
+  iterator begin() { return iterator{this, 0}; }
+  iterator end() { return iterator{this, s.length()}; }
+};
