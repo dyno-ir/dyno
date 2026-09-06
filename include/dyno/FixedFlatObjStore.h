cbafd72d060be4c6a5349bde2d1747033d63683a
Mathis Salmen <mathis.salmen@matsal.de>
2026-09-06 14:24:31 +0100

    fix

--- include/dyno/FixedFlatObjStore.h
+++ include/dyno/FixedFlatObjStore.h
@@ -10,7 +10,7 @@
 #include <optional>
 #include <type_traits>
 namespace dyno {
-#if __POINTER_WIDTH__ > 4
+#if __SIZEOF_POINTER__ > 4
 template <typename T> class FixedFlatObjStore {
 private:
   using Traits = ObjTraits<T>;
